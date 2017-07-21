e60b1239dad738a36325f33f3dbd96ccb8e07891
Vlasov Artem <avlasov@selectica.com>
2017-07-21 21:57:00 +0300

    Update app execution for Qualifier app

--- src/main/java/org/avlasov/springexamples/beanqualifying/qualifier/QualifierApp.java
+++ src/main/java/org/avlasov/springexamples/beanqualifying/qualifier/QualifierApp.java
@@ -3,6 +3,7 @@ package org.avlasov.springexamples.beanqualifying.qualifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.avlasov.springexamples.beanqualifying.entity.QualifierEntity;
+import org.avlasov.springexamples.beanqualifying.qualifier.config.QualifierAppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -14,7 +15,9 @@ public class QualifierApp {
 
     public static void main(String[] args) {
         LOGGER.info("Qualifier Example");
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(QualifierEntity.class);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(QualifierAppConfig.class);
+        QualifierEntity bean = applicationContext.getBean(QualifierEntity.class);
+        LOGGER.info("QualifierEntity bean contains qualified bean with type {}", bean.getQualifiedEntity().getClass().getName());
         applicationContext.close();
     }
 
