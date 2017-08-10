6970af7236301311c1cf5db9e921119fa67ad4b4
Vlasov Artem <avlasov@selectica.com>
2017-08-10 18:05:00 +0300

    Add Logger Test Case

---
+++ src/test/java/org/avlasov/springexamples/MainLoggerTestCase.java
@@ -0,0 +1,13 @@
+package org.avlasov.springexamples;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created By artemvlasov on 09/08/2017
+ **/
+public class MainLoggerTestCase {
+
+    public final static Logger LOGGER = LogManager.getLogger(MainLoggerTestCase.class);
+
+}
