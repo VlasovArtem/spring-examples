cae089d7a8af6cec68184be4f3814570e9a304b1
Vlasov Artem <avlasov@selectica.com>
2017-08-10 18:10:40 +0300

    Update LifecycleAppConfig

--- src/main/java/org/avlasov/springexamples/lifecycle/config/LifecycleAppConfig.java
+++ src/main/java/org/avlasov/springexamples/lifecycle/config/LifecycleAppConfig.java
@@ -2,14 +2,12 @@ package org.avlasov.springexamples.lifecycle.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by artemvlasov on 16/07/2017.
  */
 @ComponentScan("org.avlasov.springexamples.lifecycle")
 @Configuration
-@PropertySource("classpath:org.avlasov.springexamples.lifecycle")
 public class LifecycleAppConfig {
 
 }
